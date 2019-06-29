20e447053a6b60881871b23ef3aa9146aee575d8
kush225 <kushagra225@gmail.com>
2019-06-29 23:42:23 +0530

    Update 1_Strings.py

--- 1_Strings.py
+++ 1_Strings.py
@@ -4,9 +4,10 @@ greetings='Hello'
 name='kushagra'
 
 message="{0}, {1}. welcome!".format(greetings,name.upper())
-message2=f"{greetings}, {name}. welcome!"
-
 print(message)
+
+message2=f"{greetings}, {name}. welcome!"
 print(message2)
+
 print(dir(name))
 print(help(str))
